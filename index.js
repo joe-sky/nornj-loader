f4219eafdf1b6fcc4e4b2f007496f8fc32b17528
bjzhoutao <zhoutaoinfo@jd.com>
2017-02-13 16:57:32 +0800

    Parameters optimization

--- index.js
+++ index.js
@@ -39,10 +39,15 @@ module.exports = function (source) {
     comment: options.commentRule
   });
 
-  //Default conversion to compiled template functions
-  if(options.compiled == null) {
-    options.compiled = true;
+  let compiled = options.compiled,
+    compiledR = resourceOptions.compiled;
+  if(compiled == null) {  //Default conversion to compiled template functions
+    compiled = true;
   }
+  if(compiledR === 'false') {
+    compiledR = false;
+  }
+  compiled = compiledR ? true : compiled;
 
   //Set configs for expressions and filters
   if (options.exprConfig) {
@@ -76,7 +81,7 @@ module.exports = function (source) {
     if (tmpls.main.trim().length > 0) {
       const tmplKey = njUtils.uniqueKey(tmpls.main);
 
-      if (options.compiled || resourceOptions.compiled) {
+      if (compiled) {
         output += 'nj.' + getCompileFnName(options.outputH) + '(' + buildTmplFns(nj.precompile(tmpls.main, options.outputH), tmplKey) + ');';
       }
       else {
@@ -91,7 +96,7 @@ module.exports = function (source) {
         const tmplKey = njUtils.uniqueKey(tmpl);
         tmplsStr += '  ' + name + ': ';
 
-        if (options.compiled || resourceOptions.compiled) {
+        if (compiled) {
           tmplsStr += 'nj.' + getCompileFnName(options.outputH) + '(' + buildTmplFns(nj.precompile(tmpl, options.outputH), tmplKey) + ')';
         }
         else {
@@ -105,6 +110,6 @@ module.exports = function (source) {
   }
 
   return '\'use strict\';\n\n'
-    + ((options.compiled || resourceOptions.compiled) ? 'var nj = require(\'nornj\');\n\n' : '')
+    + (compiled ? 'var nj = require(\'nornj\');\n\n' : '')
     + `module.exports = ${output}`;
 };
